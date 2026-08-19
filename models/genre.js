a3260f6b281b1ffa0c5f8c3f84094961327fd915
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2026-08-19 14:56:47 +0700

    last commit

---
+++ models/genre.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    const Genre = sequelize.define("Genre", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nama: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        deskripsi: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+    }, {
+        tableName: 'genre',
+        timestamps: true
+    });
+
+    Genre.associate = (models) => {
+
+        Genre.belongsToMany(models.Komik, {
+            through: 'KomikGenre',
+            foreignKey: 'genreId',
+            otherKey: 'komikId',
+            as: 'komik'
+        });
+        
+    };
+
+    return Genre;
+};
